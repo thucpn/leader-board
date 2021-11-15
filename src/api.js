65377ff7e63873cce185852999c7b778a8c2cff8
thucpn <thucsh2@gmail.com>
2021-11-15 08:28:21 +0700

    feat: asda

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = 'https://8a32-2402-800-6388-64d3-79d5-ea84-43b0-2c8c.ngrok.io';
+const BACKEND_URL = 'https://eaa9-2402-800-6341-eafc-149-87c5-8d38-5479.ngrok.io';
 
 const api = axios.create({ baseURL: BACKEND_URL });
 
